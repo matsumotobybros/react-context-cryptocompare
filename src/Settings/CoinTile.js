cee9715d901e963d3fc1b2af916d74d9bcb89fda
noriyuki.matsumoto <noriyuki.matsumoto@uzabase.com>
2019-06-15 00:29:30 +0900

    Add TopSection CoinGrid

--- src/Settings/CoinTile.js
+++ src/Settings/CoinTile.js
@@ -1,19 +1,26 @@
 import React from "react";
 import { AppContext } from "../App/AppProvider";
-import { SelectableTile } from "../Shared/Title";
+import { SelectableTile, DeletableTile, DisableTile } from "../Shared/Title";
 import CoinHeaderGrid from "./CoinHeaderGrid";
 import CoinImage from "../Shared/CoinImage";
 
-export default function({ coinKey }) {
+export default function({ coinKey, topSection }) {
   return (
     <AppContext.Consumer>
       {({ coinList }) => {
         let coin = coinList[coinKey];
 
-        const TitleClass = SelectableTile;
+        let TitleClass = SelectableTile;
+        if (topSection) {
+          TitleClass = DeletableTile;
+        }
         return (
           <TitleClass>
-            <CoinHeaderGrid name={coin.CoinName} symbol={coin.Symbol}>
+            <CoinHeaderGrid
+              topSection={topSection}
+              name={coin.CoinName}
+              symbol={coin.Symbol}
+            >
               {coin.CoinName}
             </CoinHeaderGrid>
             <CoinImage coin={coin} />
