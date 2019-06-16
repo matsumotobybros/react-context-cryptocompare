2ef72c31013825b2a1c517e219af4f18788141b3
noriyuki.matsumoto <noriyuki.matsumoto@uzabase.com>
2019-06-17 00:53:43 +0900

    Add Hight chart theme

--- src/Dashboard/PriceChart.js
+++ src/Dashboard/PriceChart.js
@@ -3,6 +3,9 @@ import React from 'react';
 import { Tile } from '../Shared/Tile';
 import { AppContext } from '../App/AppProvider';
 import ReactHightcharts from 'react-highcharts';
+import HightChartTheme from './HighChartTheme';
+
+ReactHightcharts.Highcharts.setOptions(HightChartTheme);
 
 export default function() {
   return (
